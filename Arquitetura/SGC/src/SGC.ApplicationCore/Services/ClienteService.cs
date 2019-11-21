831130b206ffcd15d68c66bdf769f20903da4042
jackson Bruno M.P Moraes <jacksonbruno@hotmail.com>
2019-11-21 19:08:59 -0300

    aula 8 - adiciona especialidades das entidades para repository e implementa a camada de servicos clienteService contatoService
    https://www.youtube.com/watch?v=mMpXuctRC1w

---
+++ Arquitetura/SGC/src/SGC.ApplicationCore/Services/ClienteService.cs
@@ -0,0 +1,49 @@
+﻿using SGC.ApplicationCore.Entity;
+using SGC.ApplicationCore.Interfaces.Repository;
+using SGC.ApplicationCore.Interfaces.Servicess;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGC.ApplicationCore.Services
+{
+    public class ClienteService : IClienteService
+    {
+        private readonly IClienteRepository _clienteRepository;
+        public ClienteService(IClienteRepository clienteRepository)
+        {
+            _clienteRepository = clienteRepository;
+        }
+        public Cliente Adicionar(Cliente entity)
+        {
+            //TODO: adicionar regra de negocios antes
+            return _clienteRepository.Adicionar(entity);
+        }
+
+        public void Atualizar(Cliente entity)
+        {
+            _clienteRepository.Atualizar(entity);
+        }
+
+        public IEnumerable<Cliente> Buscar(Expression<Func<Cliente, bool>> predicado)
+        {
+            return _clienteRepository.Buscar(predicado);
+        }
+
+        public Cliente ObterPorId(int id)
+        {
+            return _clienteRepository.ObterPorId(id);
+        }
+
+        public IEnumerable<Cliente> ObterTodos()
+        {
+            return _clienteRepository.ObterTodos();
+        }
+
+        public void Remover(Cliente entity)
+        {
+            _clienteRepository.Remover(entity);
+        }
+    }
+}
